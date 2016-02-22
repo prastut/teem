3c5acbd003a9c8db24ff26ea1357142b5e973f45
David Llop <llop-no-spam-please@protonmail.com>
2016-02-22 22:45:39 +0100

    Upgrade (get|update)UserProfile API

--- src/js/controllers/profile.js
+++ src/js/controllers/profile.js
@@ -16,13 +16,14 @@ angular.module('Teem')
         controller: 'ProfileCtrl'
       });
   }])
-  .controller('ProfileCtrl', ['$scope', 'SessionSvc', '$timeout', function ($scope, SessionSvc, $timeout) {
+  .controller('ProfileCtrl', ['$scope', 'SessionSvc', function ($scope, SessionSvc) {
     SessionSvc.loginRequired(function() {
       $scope.user = SessionSvc.users.current();
       $scope.rawAvatar = '';
       $scope.croppedAvatar = '';
       $scope.cropping = false;
       $scope.avatar = '';
+      $scope.transparent = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=';
 
       function handleFileSelect(evt) {
         $scope.cropping = true;
@@ -38,33 +39,35 @@ angular.module('Teem')
       angular.element(document.querySelector('#avatar')).on('change', handleFileSelect);
 
       SessionSvc.getUserProfile(function(res) {
-        if (res.data.avatar_url) {
+        if (res.data.avatarUrl) {
           $scope.$apply(function() {
-            $scope.avatar = 'http://localhost:9898' + res.data.avatar_url;
+            $scope.avatar = res.data.avatarUrl;
           });
         }
       });
 
       $scope.saveAvatar = function() {
         $scope.cropping = false;
-        SessionSvc.updateUserProfile({avatar_data: $scope.croppedAvatar}, function (res) {
+        SessionSvc.updateUserProfile({avatarData: $scope.croppedAvatar}, function (res) {
           if (res.error) {
             return;
           }
           $scope.$apply(function() {
-            $scope.avatar = 'http://localhost:9898' + res.data.avatar_url;
+            $scope.avatar = res.data.avatarUrl;
           });
         });
       };
 
       $scope.deleteAvatar = function() {
-        SessionSvc.updateUserProfile({avatar_data: false}, function(res) {
+        SessionSvc.updateUserProfile({avatarData: ''}, function(res) {
           if(res.error) {
             return;
           }
-          $scope.rawAvatar = '';
-          $scope.croppedAvatar = '';
-          $scope.avatar = '';
+          $scope.$apply(function() {
+            $scope.rawAvatar = '';
+            $scope.croppedAvatar = '';
+            $scope.avatar = '';
+          });
         });
       };
     });
